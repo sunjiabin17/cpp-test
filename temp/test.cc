bdd106165df0488bce391a8da696657b4d723186
sunjiabin <sunjiabin01@outlook.com>
2023-09-27 21:44:34 +0800

    update

--- temp/test.cc
+++ temp/test.cc
@@ -17,38 +17,35 @@ public:
 
 Foo func1() {
     Foo foo;
-    // std::cout << "func1" << std::endl;
+    std::cout << &foo << std::endl;
     return foo;
 }
 
 Foo func2() {
     Foo foo;
-    // std::cout << "func2" << std::endl;
+    std::cout << &foo << std::endl;
     return std::move(foo);
 }
 
 Foo&& func3() {
     Foo foo;
-    // std::cout << "func3" << std::endl;
+    std::cout << &foo << std::endl;
     return std::move(foo);
 }
 
 int main() {
     // Foo a1 = func1();
     // std::cout << &a1 << std::endl;
-    // std::cout << a1.x << std::endl;
-    // std::cout << "----------------" << std::endl;
+
     // Foo a2 = func2();
     // std::cout << &a2 << std::endl;
-    // std::cout << a2.x << std::endl;
-    // std::cout << "----------------" << std::endl;
-    // Foo&& a3 = func3();
-    // std::cout << &a3 << std::endl;
-    // std::cout << a3.x << std::endl;
+    
+    Foo&& a3 = func3();
+    std::cout << &a3 << std::endl;
 
-    int x = 123;
-    int&& y = std::move(x);
-    std::cout << x << std::endl;
-    std::cout << y << std::endl;
+    // int x = 123;
+    // int&& y = std::move(x);
+    // std::cout << &x << std::endl;
+    // std::cout << &y << std::endl;
     return 0;
 }
