6e860d5078b6b67664b661f3250293274ac7d621
sunjiabin <sunjiabin01@outlook.com>
2023-10-13 22:07:36 +0800

    update

--- operators/deleter/deleter.cc
+++ operators/deleter/deleter.cc
@@ -1,11 +1,34 @@
+#include <algorithm>
 #include <iostream>
 #include <memory>
 
 class Foo {
 public:    
     int x;
-    Foo(int _x): x(_x) {std::cout << "Foo(int)" << std::endl;}
-    ~Foo() {std::cout << "~Foo()" << std::endl;}
+    Foo(int _x): x(_x) {
+        std::cout << "Foo(int)" << std::endl;
+        }
+    ~Foo() {
+        std::cout << "~Foo()" << std::endl;
+        }
+    Foo(Foo&& foo) {
+        x = foo.x;
+        std::cout << "move" << std::endl;
+        }
+    Foo(const Foo& foo) {
+        x = foo.x;
+        std::cout << "copy" << std::endl;
+        }
+    Foo operator=(Foo&& foo) {
+        x = foo.x;
+        std::cout << "operator=(Foo&&)" << std::endl;
+        return *this;
+        }
+    Foo operator=(Foo& foo) {
+        x = foo.x;
+        std::cout << "operator=(Foo&)" << std::endl;
+        return *this;
+        }
 };
 
 struct deleter {
@@ -21,9 +44,23 @@ void func_deleter(Foo* p) {
     delete p;
 }
 
+void print_address(void* a) {
+    std::cout << "address a: " << a << std::endl;
+    std::cout << "address &a: " << &a << std::endl;
+    std::cout << "-----------" << std::endl;
+}
+
+
 int main() {
     // std::unique_ptr<Foo, deleter> a(new Foo(1));
     // std::unique_ptr<Foo, void(*)(Foo*)> a(new Foo(1), func_deleter);
-    std::unique_ptr<Foo, decltype(func_deleter)*> a(new Foo(1), func_deleter);
+    // int* a = new int(1);
+    // deleter()(a);
+    // std::unique_ptr<Foo, decltype(func_deleter)*> a(new Foo(255), func_deleter);
+    // a.reset();
+
+    // Foo* a = new Foo(1);
+    // func_deleter(a);
+
     return 0;
 }
