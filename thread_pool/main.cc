0a6297951422f4311fcab3d574ba9de088a7b508
sunjiabin <sunjiabin01@outlook.com>
2023-11-05 23:15:11 +0800

    thread pool test

--- thread_pool/main.cc
+++ thread_pool/main.cc
@@ -5,15 +5,13 @@
 int main(int argc, char** argv) {
   {  
     ThreadPool pool(4);
-
     for (int i = 0; i < 10; ++i) {
       pool.Enqueue([i]() {
         std::cout << "executing task in thread " << std::this_thread::get_id()
                   << " with index " << i << std::endl;
-        
       });
     }
-  }
+  } // ~ThreadPool() will be called here
   std::cout << "main thread exits" << std::endl;
   return 0;
 }
