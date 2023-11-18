f4a4bb3a8a023aaefd2a865053898f29b8553552
sunjiabin <sunjiabin01@outlook.com>
2023-11-18 20:52:03 +0800

    std::back_inserter

--- std/std_inserter.cc
+++ std/std_inserter.cc
@@ -9,5 +9,12 @@ int main() {
   b.reserve(a.size());
   std::copy(a.begin(), a.end(), std::back_inserter(b));
   std::copy(b.begin(), b.end(), std::ostream_iterator<int>(std::cout, " "));
+
+  std::vector<int> c;
+  c.reserve(a.size());
+  std::copy(a.begin(), a.end(), std::inserter(c, c.begin()));
+  for (auto i : c) {
+    std::cout << i << " ";
+  }
   return 0;
 }
